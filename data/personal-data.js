8b7ce010a5c141b721044e1ac89dc968ee87cfd8
Firstjs <tirawutrodnak@gmail.com>
2025-04-02 15:02:25 +0700

    My first commit

---
+++ data/personal-data.js
@@ -0,0 +1,90 @@
+const personalData = {
+    websiteLogo: 'website logo.png', //Add your website-logo in "icons" folder
+    name: 'Tirawut Rotnak',
+    photo: 'meowish_profile.png', //Add your photo in "images" folder
+    profession: 'Student',
+    email: 'tirawutrodnak@gmail.com',
+    contactNumber: '0964032620',
+    adress: 'Bangkok, Thailand',
+    fbLink: 'https://www.facebook.com/EmprerorDFirst',
+    instagramLink: 'https://www.instagram.com/first_tira/',
+    tiktokLink: 'https://www.tiktok.com/@firsttira',
+    aboutMe: 'ผมเป็นนักเรียนbootcampที่เรียนสาย FullStack Developer ผมได้สร้างเว็บไซต์นี้ขึ้นมาโดยคำนึงถึงความคิดสร้างสรรค์ ความทุ่มเท และประสบการณ์ของผู้ใช้ ผมได้สร้างเว็บไซต์นี้เพื่อ ฉันเปิดใจในการขายให้กับผู้ซื้อที่เหมาะสมซึ่งเห็นคุณค่าของการออกแบบ ฟังก์ชันการทำงาน และโอกาสในการเติบโตที่เว็บไซต์นี้มอบให้',
+    skills: [ {
+                name: 'HTML',
+                icon: 'html.png', //Add your skill-icon in "icons" folder
+                mastery: 'Advanced',
+                type: 'tech-stack'
+              },
+              {
+                name: 'CSS',
+                icon: 'CSS.png', //Add your skill-icon in "icons" folder
+                mastery: 'Advanced',
+                type: 'tech-stack'
+              },
+              {
+                name: 'JS',
+                icon: 'js (1).png', //Add your skill-icon in "icons" folder
+                mastery: 'Advanced',
+                type: 'tech-stack'
+              },
+              {
+                name: 'PHP', 
+                icon: 'php.png', //Add your skill-icon in "icons" folder
+                mastery: 'Beginner',
+                type: 'tech-stack'
+              },
+              {
+                name: 'REACT', 
+                icon: 'React (2).png', //Add your skill-icon in "icons" folder
+                mastery: 'Beginner',
+                type: 'tech-stack'
+              },
+              {
+                name: 'Node.js', 
+                icon: 'nodeJS icon.png', //Add your skill-icon in "icons" folder
+                mastery: 'Beginner',
+                type: 'tech-stack'
+              },
+              {
+                name: 'MongoDB',
+                icon: 'mongoDB icon.png', //Add your skill-icon in "icons" folder
+                mastery: 'Beginner',
+                type: 'tech-stack'
+              },
+              {
+                name: 'VS code',
+                icon: 'visual-studio.png', //Add your skill-icon in "icons" folder
+                mastery: 'Intermediate',
+                type: 'tools'
+              },
+              {
+                name: 'GitHub',
+                icon: 'github (2).png', //Add your skill-icon in "icons" folder
+                mastery: 'Intermediate',
+                type: 'tools'
+              },
+              {
+                name: 'Git',
+                icon: 'Git.png', //Add your skill-icon in "icons" folder
+                mastery: 'Intermediate',
+                type: 'tools'
+              },
+              {
+                name: 'Figma',
+                icon: 'figma icon.png', //Add your skill-icon in "icons" folder
+                mastery: 'Intermediate',
+                type: 'tools'
+              },
+              {
+                name: 'CapCut',
+                icon: 'capcut-icon.png', //Add your skill-icon in "icons" folder
+                mastery: 'Intermediate',
+                type: 'tools'
+              }
+            ],
+  }
+  
+  
+  
+  export default personalData;
